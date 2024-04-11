85415b62deb23890c4f8b183bf0c7853208ddc9e
cottachi288 <cottachi288@gmail.com>
2024-04-11 09:28:31 +0700

    install cors

--- src/Server.js
+++ src/Server.js
@@ -2,14 +2,21 @@ import express from "express";
 import { connect} from "mongoose";
 import router from "./routes/index.js";
 import dotenv from "dotenv";
+import cors from "cors"
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT;
 const URI_DB = process.env.URI_DB;
-
 connect(URI_DB);
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json())
 app.use("/api", router);
