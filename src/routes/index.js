b0d366dbff3d0bef9bd9681f734df31e7d1b27de
cottachi288 <cottachi288@gmail.com>
2024-04-12 14:01:29 +0700

    update productDetailsRoutes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import express from "express"
-import routerProducts from "./productRoutes.js"
+import { routerProducts} from "./productRoutes.js";
+import {routerProductDetails} from "./productDetailsRoutes.js"
 const router = express.Router()
 
 router.use('/products', routerProducts)
+router.use('/productDetails', routerProductDetails )
 export default router
